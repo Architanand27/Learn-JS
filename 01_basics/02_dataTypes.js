1c848c9f46794c1c0d5660c25819a2ef1f35b145
Archit Anand <153591992+Architanand27@users.noreply.github.com>
2025-10-06 13:34:57 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) //this command is wrong because we are using nodejs , not browser
+
+console.log(3+3)
+
+//code redability should be higher
+
+console.log("Arxchit")
+
+// dataTypes in JS
+let name="Archit"
+let age= 18
+let isLoggedIn=true
+
+// number => approx 2 to power 53
+// bigint => if the vale is larger than number daataType then its use it is used in big website generally
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => it is used for uniqueness
+
+// object 
+
+
+
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
