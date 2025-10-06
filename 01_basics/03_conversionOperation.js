c513b0850975a35726ba6fb15bd14517e1f8d7f4
Archit Anand <153591992+Architanand27@users.noreply.github.com>
2025-10-06 13:57:23 +0000

    conversionOfDtataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="27"
+
+
+// console.log(typeof score);
+// console.log(typeof(score)); //both are use to checks data types of variable
+
+let  valueInNumber= Number(score) // it convert anoter data types into number
+
+// In conversion
+// "27" => 27
+// "27asdf" => NaN (not a number)
+// true => 1; false => 0;
+// null => 0
+// undefined => NaN
+// console.log(typeof valueInNumber)
+
+
+
+// lets check how number convert into a boolean
+
+
+let isLoggedIn =9 // If we enter any string or num it shows true but if we enter either 0 or keeps empty variable it convert into false
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
