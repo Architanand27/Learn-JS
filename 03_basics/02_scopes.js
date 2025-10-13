e09ffbddbff368021837f44d03e9d3d52c375160
Archit Anand <153591992+Architanand27@users.noreply.github.com>
2025-10-13 18:04:09 +0000

    scopeInJavaScript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,4 +6,26 @@
 
 {}// this is a block scope 
 
+function one(){
+    const username = "Archit"; // function scope
+    function two(){
+        const website = "youtube.com";
+        console.log(username); // can access parent scope
+    }
+    // console.log(website); // error: website is not defined
+    two();
+}
+// one();
+
+if(true){
+    const username = "Archit";
+    if(username === "Archit"){
+        const website = "youtube.com";
+        // console.log(username); // can access parent scope
+    }
+}
+
+
+
+
 
